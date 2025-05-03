a031322e0f14e71f36425ee8e8f33e6b14541646
codewithcobby <frank.k.abrokwa@gmail.com>
2025-05-03 20:55:52 +0000

    Fix vercel export issue

--- logic/config/base.ts
+++ logic/config/base.ts
@@ -2,7 +2,7 @@ import axios from "axios"
 import url from "./url"
 
 const axiosInstance = axios.create({
-  baseURL: url.environment === "development" ? url.users_local : url.users_live, // Add your actual base URL
+  baseURL: url.server, // Add your actual base URL
 })
 
 export default axiosInstance
--- logic/config/url.ts
+++ logic/config/url.ts
@@ -1,6 +1,5 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-  users_live: process.env.NEXT_PUBLIC_SERVER_URL || "",
-  users_local: "http://localhost:1337/api/",
+  server: process.env.NEXT_PUBLIC_SERVER_URL || "",
   environment: process.env.NODE_ENV,
 }
