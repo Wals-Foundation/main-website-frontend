b9be3b9bba764592b18a86da04ce295c5bbedab0
codewithcobby <frank.k.abrokwa@gmail.com>
2025-05-03 20:55:43 +0000

    Fix vercel export issue

--- logic/config/url.ts
+++ logic/config/url.ts
@@ -1,6 +1,6 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-  users_live: "https://live/api/",
+  users_live: process.env.NEXT_PUBLIC_SERVER_URL || "",
   users_local: "http://localhost:1337/api/",
   environment: process.env.NODE_ENV,
 }
