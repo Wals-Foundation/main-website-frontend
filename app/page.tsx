bb7294f351e430245ccb3e06d968dac5923cafb8
codewithcobby <f.abrokwa@haxolabs.com>
2025-06-25 18:23:33 +0000

    update slider height

--- app/page.tsx
+++ app/page.tsx
@@ -144,7 +144,7 @@ export default function Home() {
       </section>
 
       {pageControlSlugMap.get("home_hero_carousel") && (
-        <section className="pt-10 relative  h-[100vh]">
+        <section className="pt-10 relative min-h-[932px] md:min-h-[784px]">
           <div className="absolute top-0 left-0 right-0 z-10 px-4 md:px-0">
             <div className="max-w-[1440px] mx-auto py-10 md:py-16 md:right-10">
               <div className="w-full md:w-11/12 mx-auto relative h-full">
@@ -223,7 +223,7 @@ export default function Home() {
 
                 return (
                   <SwiperSlide key={n}>
-                    <div className="relative w-full h-screen">
+                    <div className="relative w-full min-h-[932px] md:min-h-[784px]">
                       {/* Mobile image */}
                       {mobileImageUrl && (
                         <img
