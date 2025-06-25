efee0ce84ce140cd844f10d894fc0508aa42d602
codewithcobby <f.abrokwa@haxolabs.com>
2025-06-25 18:38:09 +0000

    fix cause card

--- components/CausesCard.tsx
+++ components/CausesCard.tsx
@@ -23,31 +23,38 @@ const CausesCard: React.FC<CausesCardProps> = (props) => {
   const pageControlSlugMap = createSlugMapForControl(data.pageControl)
 
   return (
-    <div className="lg:max-w-[1052px] mx-auto bg-white p-6 md:flex justify-between items-start rounded-2xl w-full">
-      <div className="w-full">{image && <img src={image} alt={title} className="rounded-xl w-full lg:w-auto" />} </div>
-      <div className="lg:max-w-[482px] h-[429px] flex flex-col justify-between items-center w-full">
-        <div className="w-full">
-          <Typography type="Subtitle" className="text-left text-2xl">
-            {title}
-          </Typography>
-          <Typography type="Custom">{subtitle}</Typography>
-        </div>
-        <div className="border-t border-light-gray pt-5  w-full">
-          {pageControlSlugMap.get("cause_card_impact_text") && <Typography type="Custom">{content}</Typography>}
-          {pageControlSlugMap.get("cause_card_button_2") && (
-            <div className="pt-4 lg:flex items-center">
-              <div className="pb-4 lg:pb-0 lg:pr-3">
+    <div className="lg:max-w-[1052px] mx-auto bg-white p-6 rounded-2xl w-full">
+      <div className="flex flex-col lg:flex-row gap-6 items-start">
+        {/* Image Section */}
+        {image && (
+          <div className="w-full lg:w-1/2">
+            <img src={image} alt={title} className="rounded-xl w-full object-cover" />
+          </div>
+        )}
+
+        {/* Text Section */}
+        <div className="w-full lg:w-1/2 flex flex-col justify-between h-full">
+          <div>
+            <Typography type="Subtitle" className="text-left text-2xl">
+              {title}
+            </Typography>
+            <Typography type="Custom">{subtitle}</Typography>
+          </div>
+          <div className="border-t border-light-gray pt-5 mt-4">
+            {pageControlSlugMap.get("cause_card_impact_text") && <Typography type="Custom">{content}</Typography>}
+            {pageControlSlugMap.get("cause_card_button_2") && (
+              <div className="pt-4 flex flex-col lg:flex-row items-start lg:items-center gap-4">
                 <Link href={`/${isDev ? "causes/cause?" : "causes/cause.html?"}${id}`}>
                   <Button theme="primary" title="Read More" />
                 </Link>
+                {displayDonateButton && (
+                  <Link href="/donate">
+                    <Button theme="secondary" title="Make a Donation" />
+                  </Link>
+                )}
               </div>
-              {displayDonateButton && (
-                <Link href="/donate">
-                  <Button theme="secondary" title="Make a Donation" />
-                </Link>
-              )}
-            </div>
-          )}
+            )}
+          </div>
         </div>
       </div>
     </div>
